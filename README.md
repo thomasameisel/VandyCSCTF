0728f4b9411b09818a0fcaa7649ad155397bfa1d
Tommy Meisel <thomas.meisel95@gmail.com>
2016-09-25 00:54:11 -0500

    add sql injection challenge

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Done for now
 - cookies - website that change the cookies in order to login using admin
 - post to url - website that exposes the url that gets the flag, no authentication server side when posting to that url
 - login in source - have username and password in the source code
+- sql - sql injection to login
 
 Ideas
 - php - website that saves a file using php, use php vulnerabilities to execute php that will show the flag
-- sql - sql injection to login
 - robots - encrypted flag in the robots.txt file
 - users - users.js is on the server, brings to flag
---
+++ sql_injection/index.js
@@ -0,0 +1,37 @@
+/*jslint node: true */
+/*jslint esversion: 6 */
+"use strict";
+
+let express = require('express');
+let bodyParser = require('body-parser');
+let logger = require('morgan');
+let sqlite3 = require('sqlite3');
+
+let db = new sqlite3.Database(__dirname + '/users.db');
+let app = express();
+app.use(express.static('public'));
+app.use(logger('combined'));
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/v1/session', function(req, res) {
+  let username = req.body.username;
+  let password = req.body.password;
+  db.all('SELECT * FROM users WHERE username="' + username + '" AND password="' + password + '"',
+    function(err, rows) {
+      if (err) res.status(400).send({ error: 'Error with database' });
+      else if (rows.length === 0) {
+        res.status(401).send({ error: 'Username and password are incorrect' });
+      } else {
+        res.status(201).send({ flag: 'sanitize_your_inputs' });
+      }
+    });
+});
+
+app.post('/v1/flag', function(req, res) {
+  res.status(201).send({ flag: 'post_to_me' });
+});
+
+let server = app.listen(8080, function () {
+    console.log('Example app listening on ' + server.address().port);
+});
