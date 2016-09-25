9e008b2eecdb2eb87d95f78e36d45551f6b82cb4
Tommy Meisel <thomas.meisel95@gmail.com>
2016-09-25 00:11:59 -0500

    update cookies challenge validation js

--- cookies/public/js/validation.js
+++ cookies/public/js/validation.js
@@ -7,10 +7,10 @@
 
 function getCookie(c_name) {
   if (document.cookie.length > 0) {
-    let c_start = document.cookie.indexOf(c_name + "=");
+    let c_start = document.cookie.indexOf(c_name + '=');
     if (c_start != -1) {
       c_start = c_start + c_name.length + 1;
-      let c_end = document.cookie.indexOf(";", c_start);
+      let c_end = document.cookie.indexOf(';', c_start);
       if (c_end == -1) {
         c_end = document.cookie.length;
       }
@@ -46,9 +46,9 @@ function changeError(msg) {
   return false;
 }
 
-function addCookiesAndLoad(data) {
-  document.cookie = 'username=' + data.username + ';';
-  document.cookie = 'auth=true';
+function addCookiesAndLoad(username) {
+  document.cookie = 'username=' + username + ';';
+  document.cookie = 'auth=true;';
   window.location.href = '/profile.html';
 }
 
@@ -74,7 +74,7 @@ function submitLogin() {
   let info = inputToJSON('login');
 
   ajaxPost('/v1/session', info,
-    addCookiesAndLoad,
+    (data) => addCookiesAndLoad(data.username),
     changeError);
 }
 
