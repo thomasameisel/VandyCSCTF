56dbc822dc344f4aa5f62c7ea73426e218218fd1
Patrick <patrickkerry.pei@gmail.com>
2016-09-27 22:47:38 -0500

    Refactor php_file challenge

--- php_file_flag/public/index.php
+++ php_file_flag/public/index.php
@@ -1,54 +1,53 @@
 <?php
-  session_start();
+    session_start();
 
-  if ($_FILES["dispic"]["error"] > 0) {
-    echo "<p>Error: " . $_FILES["dispic"]["error"] . "</p>";
-  }
-  else
-  {
-    $dest_dir = "uploads/";
-    $dest = $dest_dir . basename($_FILES["dispic"]["name"]);
-    $src = $_FILES["dispic"]["tmp_name"];
-    if (move_uploaded_file($src, $dest)) {
-      $_SESSION["dispic_url"] = $dest;
-      chmod($dest, 0644);
-      echo "<p>Successfully uploaded your display picture.</p>";
+    if ($_FILES["dispic"]["error"] > 0) {
+        echo "<p>Error: " . $_FILES["dispic"]["error"] . "</p>";
+    } else {
+        $dest_dir = "uploads/";
+        $dest     = $dest_dir . basename($_FILES["dispic"]["name"]);
+        $src      = $_FILES["dispic"]["tmp_name"];
+        if (move_uploaded_file($src, $dest)) {
+            $_SESSION["dispic_url"] = $dest;
+            chmod($dest, 0644);
+            echo "<p>Successfully uploaded your display picture.</p>";
+        }
     }
-  }
 
-  $url = "https://upload.wikimedia.org/wikipedia/commons/f/f8/" .
-         "Question_mark_alternate.svg";
-  if (isset($_SESSION["dispic_url"])) {
-    $url = $_SESSION["dispic_url"];
-  }
+    $url = "./images/vandycs-logo.png";
+
+    if (isset($_SESSION["dispic_url"])) {
+        $url = $_SESSION["dispic_url"];
+    }
 
 ?>
 
 <html>
-  <head>
-    <title>VandyCS CTF</title>
-  </head>
-  <body>
-    <center>
-      <h1>Welcome to VandyCS' CTF Social Network!</h1>
-      <div>
-        <img height="50%" width="50%" src=<?php echo $url; ?> />
-        <?php
-          if (!isset($_SESSION["dispic_url"])) {
-            echo "<p>Oh, looks like you don't have a profile image" .
-                 " -- upload one now!</p>";
-          }
-        ?>
-        <form action="" method="post" enctype="multipart/form-data">
-          <input type="file" name="dispic" size="40" />
-          <input type="submit" value="Upload!">
-        </form>
 
-        <p>
-           Password (accessible only to members of the club):
-           <a href="password.txt">password.txt</a>
-        </p>
-      </div>
-    </center>
-  </body>
+    <head>
+        <title>VandyCS CTF</title>
+    </head>
+
+    <body>
+        <center>
+            <h1>VandyCS CTF!</h1>
+            <div>
+                <img height="50%" width="30%" src=<?php echo $url;?> />
+                <?php
+                    if (!isset($_SESSION["dispic_url"])) {
+                        echo "<p>Upload custom image</p>";
+                    }
+                    ?>
+                <form action="" method="post" enctype="multipart/form-data">
+                    <input type="file" name="dispic" size="40" />
+                    <input type="submit" value="Image upload">
+                </form>
+                <p>
+                    Hidden flag:
+                    <a href="flag.txt">flag.txt</a>
+                </p>
+            </div>
+        </center>
+
+    </body>
 </html>
